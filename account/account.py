e62c58abc84b76aca5758788c84842093929b7e8
Kaisar04 <sarymsakovkaisar@gmail.com>
2021-12-25 15:34:15 +0500

    Account to json, from json

--- account/account.py
+++ account/account.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 from decimal import Decimal
 from uuid import UUID
@@ -6,16 +7,34 @@ from uuid import UUID
 class CurrencyMismatchError(ValueError):
     pass
 
+
 @dataclass
 class Account:
     id_: UUID
     currency: str
     balance: Decimal
 
-    def __lt__(self, other):
+    def __lt__(self, other: "Account"):
         assert isinstance(other, Account)
         if self.currency != other.currency:
             raise CurrencyMismatchError
         return self.balance < other.balance
+
+    def to_json(self) -> str:
+        json_repr = {
+            "id": str(self.id_),
+            "currency": self.currency,
+            "balance": float(self.balance),
+        }
+        return json.dumps(json_repr)
+
+    @classmethod
+    def from_json(cls, json_str: str) -> "Account":
+        obj = json.loads(json_str)
+        return Account(
+            id_=UUID(obj["id"]),
+            currency=obj["currency"],
+            balance=Decimal(obj["balance"]),
+        )
 
 
--- account/test_account.py
+++ account/test_account.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 import pytest
+import json
 
 from account.account import Account, CurrencyMismatchError
 
@@ -39,3 +40,32 @@ class TestAccount:
 
         with pytest.raises(CurrencyMismatchError):
             assert account2 < account
+
+    def test_json_import_export(self) -> None:
+        account_id = uuid4()
+        account = Account(
+            id_=account_id,
+            currency="KZT",
+            balance=Decimal(10),
+        )
+
+        json_account = account.to_json()
+        assert json.loads(json_account) == {
+            "id": str(account.id_),
+            "currency": account.currency,
+            "balance": account.balance,
+        }
+
+    def test_account_from_json(self) -> None:
+        test_json = '{"id": "4dc2e5e3-671f-4efb-8a07-bbb71147a335", "currency": "KZT", "balance": 10.0}'
+
+        account = Account.from_json(test_json)
+        assert isinstance(account, Account)
+        assert account.id_ == UUID("4dc2e5e3-671f-4efb-8a07-bbb71147a335")
+        assert account.balance == Decimal(10)
+        assert account.currency == "KZT"
+
+
+
+
+
